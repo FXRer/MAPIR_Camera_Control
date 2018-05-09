3bc91f1cb74561a9361c3f2338e0507515a90dee
mapircomments <39131306+mapircomments@users.noreply.github.com>
2018-05-09 10:57:15 -0700

    Comments to Camera Control
    added comments to the camera control main method and commented out the lines of code that are meant to display a loading screen (Will come back for those later)

--- MAPIR_Camera_Control.py
+++ MAPIR_Camera_Control.py
@@ -4,30 +4,39 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from MAPIR_Processing_dockwidget import *
 
+'''
+This is the main function that runs the MAPIR widget, it instantiates a QT app object and then a 
+MAPIR PRocessing Dock Widget from the MAPIR_Processing_dockwidget.py file, this object is then displayed to the user
 
+*Note: The code that is commented out is so display a loading screen to the user (doesn't work yet) and there needs to 
+be a cancel button added to this code
+'''
 if __name__ == "__main__":
         try:
 
-                app = QApplication(sys.argv)
-                splash_pix = QPixmap(os.path.dirname(os.path.realpath(__file__)) + 'lut_legend_rgb.jpg')
+                app = QApplication(sys.argv) #instantiate QT app object
 
-                splash = QSplashScreen(splash_pix, Qt.WindowStaysOnTopHint)
-                splash.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
-                splash.setEnabled(True)
-                progressBar = QProgressBar(splash)
-                progressBar.setMaximum(30)
-                progressBar.setGeometry(0, splash_pix.height() - 50, splash_pix.width(), 20)
-                splash.show()
-                for i in range(1, 31):
-                        progressBar.setValue(i)
-                        t = time.time()
-                        while time.time() < t + 0.1:
-                                app.processEvents()
+                #splash_pix = QPixmap(os.path.dirname(os.path.realpath(__file__)) + 'lut_legend_rgb.jpg')
+
+                #splash = QSplashScreen(splash_pix, Qt.WindowStaysOnTopHint)
+                #splash.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+                #splash.setEnabled(True)
+                #progressBar = QProgressBar(splash)
+                #progressBar.setMaximum(30)
+                #progressBar.setGeometry(0, splash_pix.height() - 50, splash_pix.width(), 20)
+                #splash.show()
+                #for i in range(1, 31):
+                #       progressBar.setValue(i)
+                #       t = time.time()
+                #       while time.time() < t + 0.1:
+                #               app.processEvents()
 
 
+                '''create a processing dock widget app 
+                the app is defined in the MAPIR_PRocessing_dockwidget.py file'''
                 myapp = MAPIR_ProcessingDockWidget()
-                myapp.show()
-                splash.finish(myapp)
+                myapp.show() #Show the MAPIR gui
+                #splash.finish(myapp) #end the loading screen
 
         except Exception as e:
                 print(e)
